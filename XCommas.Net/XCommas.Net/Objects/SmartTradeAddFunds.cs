9c9059b7a078f7a4ab79f442f7082db4999597a6
Loic Ploumen <loicploumen@hotmail.com>
2019-08-08 22:14:57 +0200

    Added the ability to add funds to an open trade

---
+++ XCommas.Net/XCommas.Net/Objects/SmartTradeAddFunds.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace XCommas.Net.Objects
+{
+    public class SmartTradeAddFundsParameters
+    {
+        [JsonProperty("quantity")]
+        public decimal Quantity { get; set; }
+        [JsonProperty("is_market")]
+        public bool IsMarket { get; set; }
+        [JsonProperty("rate")]
+        public decimal? Rate { get; set; }
+        [JsonProperty("smart_trade_id")]
+        public int SmartTradeId { get; set; }
+        [JsonProperty("response_type")]
+        public string ResponseType { get; set; } = "step";
+    }
+}
