6821c01feeaabd9e9ef9a7a441557a9a48610364
capricciobob <robert.thomas@capricciosoftware.com>
2021-02-26 20:12:54 -0500

    Feature request - Add funds for deals #23

---
+++ XCommas.Net/XCommas.Net/Objects/DealAddFundsParameters.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace XCommas.Net.Objects
+{
+    public class DealAddFundsParameters
+    {
+        [JsonProperty("quantity")]
+        public decimal Quantity { get; set; }
+        [JsonProperty("is_market")]
+        public bool IsMarket { get; set; }
+        [JsonProperty("rate")]
+        public decimal? Rate { get; set; }
+        [JsonProperty("deal_id")]
+        public int DealId { get; set; }
+        [JsonProperty("response_type")]
+        public string ResponseType { get; set; } = "deal";
+    }
+}
