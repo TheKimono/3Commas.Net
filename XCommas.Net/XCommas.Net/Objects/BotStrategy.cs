8bd748969d10c960ab988f254d687814854f3f7a
Marc Drexler <mail@marc-drexler.de>
2022-01-03 22:37:53 +0100

    new start conditions added

--- XCommas.Net/XCommas.Net/Objects/BotStrategy.cs
+++ XCommas.Net/XCommas.Net/Objects/BotStrategy.cs
@@ -106,6 +106,18 @@ namespace XCommas.Net.Objects
         public RsiOptions Options { get; set; }
     }
 
+    public class Rsi14BotStrategy : BotStrategy
+    {
+        public const string Id = "rsi14";
+        public Rsi14BotStrategy() : base(Id)
+        {
+
+        }
+
+        [JsonProperty("options")]
+        public RsiOptions Options { get; set; }
+    }
+
     public class UltBotStrategy : BotStrategy
     {
         public const string Id = "ult";
@@ -116,5 +128,29 @@ namespace XCommas.Net.Objects
 
         [JsonProperty("options")]
         public UltOptions Options { get; set; }
+    }
+
+    public class Bb201BotStrategy : BotStrategy
+    {
+        public const string Id = "b_bands_percent1";
+        public Bb201BotStrategy() : base(Id)
+        {
+
+        }
+
+        [JsonProperty("options")]
+        public BbOptions Options { get; set; }
+    }
+
+    public class Bb202BotStrategy : BotStrategy
+    {
+        public const string Id = "b_bands_percent2";
+        public Bb202BotStrategy() : base(Id)
+        {
+
+        }
+
+        [JsonProperty("options")]
+        public BbOptions Options { get; set; }
     }
 }
--- XCommas.Net/XCommas.Net/Objects/TaPresetsType.cs
+++ XCommas.Net/XCommas.Net/Objects/TaPresetsType.cs
@@ -4,25 +4,23 @@ namespace XCommas.Net.Objects
 {
     public enum TaPresetsType
     {
-        [EnumMember(Value = "BB-20-1-LB")] 
-        BB_20_1_LB,
-        [EnumMember(Value = "BB-20-2-LB")] 
-        BB_20_2_LB,
-        [EnumMember(Value = "MFI-14\u003c40")] 
-        MFI_14_40,
-        [EnumMember(Value = "MFI-14\u003c20")] 
-        MFI_14_20,
-        [EnumMember(Value = "CCI-40\u003c-200")]
-        CCI_40_200,
-        [EnumMember(Value = "BB-20-1-UB")] 
-        BB_20_1_UB,
-        [EnumMember(Value = "BB-20-2-UB")] 
-        BB_20_2_UB,
-        [EnumMember(Value = "MFI-14\u003e65")] 
-        MFI_14_65,
-        [EnumMember(Value = "CCI-40\u003e0")] 
-        CCI_40_0,
-        [EnumMember(Value = "CCI-40\u003e100")]
-        CCI_40_100
+        [EnumMember(Value = "BB-20-1-LB")] BB_20_1_LB,
+        [EnumMember(Value = "BB-20-2-LB")] BB_20_2_LB,
+        [EnumMember(Value = "MFI-14\u003c40")] MFI_14_40,
+        [EnumMember(Value = "MFI-14\u003c20")] MFI_14_20,
+        [EnumMember(Value = "CCI-40\u003c-200")] CCI_40_200,
+        [EnumMember(Value = "MACD-12-26-9")] MACD_12_26_9,
+        [EnumMember(Value = "Parabolic-SAR")] Parabolic_SAR,
+        [EnumMember(Value = "SMA-20-50")] SMA_20_50,
+        [EnumMember(Value = "SMA-50-100")] SMA_50_100,
+        [EnumMember(Value = "SMA-100-200")] SMA_100_200,
+        [EnumMember(Value = "BB-20-1-UB")] BB_20_1_UB,
+        [EnumMember(Value = "BB-20-2-UB")] BB_20_2_UB,
+        [EnumMember(Value = "MFI-14\u003e65")] MFI_14_65,
+        [EnumMember(Value = "CCI-40\u003e0")] CCI_40_0,
+        [EnumMember(Value = "CCI-40\u003e100")] CCI_40_100,
+        [EnumMember(Value = "Heikin-Ashi-1")] Heikin_Ashi_1,
+        [EnumMember(Value = "Heikin-Ashi-3")] Heikin_Ashi_3,
+        [EnumMember(Value = "Heikin-Ashi-5")] Heikin_Ashi_5
     }
 }
