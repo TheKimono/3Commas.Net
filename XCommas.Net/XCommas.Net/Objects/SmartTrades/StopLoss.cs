75bdb9379c6f6e0769b4f83c754d0bcd703e4117
Marc Drexler <mail@marc-drexler.de>
2023-06-07 22:59:31 +0200

    fix: added breakeven property to smarttrade and added rsi14 strategy

--- XCommas.Net/XCommas.Net/Objects/SmartTrades/StopLoss.cs
+++ XCommas.Net/XCommas.Net/Objects/SmartTrades/StopLoss.cs
@@ -7,6 +7,8 @@ namespace XCommas.Net.Objects.SmartTrades
     {
         [JsonProperty("enabled")]
         public bool Enabled { get; set; }
+        [JsonProperty("breakeven")]
+        public bool Breakeven { get; set; }
         [JsonConverter(typeof(StringEnumConverter))]
         [JsonProperty("order_type")]
         public OrderType OrderType { get; set; }
